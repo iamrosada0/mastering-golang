206d07dafc0f19f8b6b96427b87446ef99e527bd
iamrosada0 <luisrosada@outlook.com>
2025-04-30 23:21:40 +0100

    feat: created the time

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main() {
 	presentTime := time.Now()
 	fmt.Println(presentTime)
 	fmt.Println(presentTime.Format("01-02-2006"))
+	fmt.Println(presentTime.Format("01-02-2006 15:04:03 Monday"))
 
 	fmt.Println(presentTime.Format(time.RFC3339))
+
+	createdDate := time.Date(2020, time.August, 10, 23, 23, 0, 0, time.Now().Location())
+
+	fmt.Print(createdDate)
 }

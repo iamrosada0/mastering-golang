668a8e190c1b008c5809dbf46e188d0ae758cad1
iamrosada0 <luisrosada@outlook.com>
2025-04-30 16:25:45 +0100

    feat: complex number

--- 02number/main.go
+++ 02number/main.go
@@ -10,4 +10,12 @@ func main() {
 	fmt.Printf("Type of c1: %T\n", c1)
 	fmt.Printf("Type of c2: %T\n", c2)
 
+	var c3 complex64 = complex64(c1 + c2)
+	fmt.Println(" c3:", c3)
+	fmt.Printf("Type of c3: %T\n", c3)
+
+	cZero := c3 - c3
+
+	fmt.Println("cZero", cZero)
+
 }

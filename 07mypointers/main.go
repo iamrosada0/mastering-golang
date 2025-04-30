39ed79a58b0b01b4fee6ba47ea7d2f110a16a1e2
iamrosada0 <luisrosada@outlook.com>
2025-04-30 23:38:13 +0100

    feat: pointer is done

--- 07mypointers/main.go
+++ 07mypointers/main.go
@@ -9,4 +9,16 @@ func main() {
 	// var ptr *int
 
 	// fmt.Println("Value of pointers is", ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber
+	fmt.Println("Value of actual pointers is", ptr)
+
+	fmt.Println("Value of actual pointers is", *ptr)
+
+	*ptr = *ptr * 2
+
+	fmt.Println("The new value is", myNumber)
+
 }

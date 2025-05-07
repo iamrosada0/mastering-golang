48cebcace703da49c3580b25ff2b502866354756
iamrosada0 <luisrosada@outlook.com>
2025-05-07 17:13:59 +0100

    feat: first example

--- 09mystruct/main.go
+++ 09mystruct/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+func main() {
+	fmt.Println("Struct in golang")
+
+	hitesh := User{"Rosada", "rosada@go.dev", true, 16}
+
+	fmt.Println(hitesh)
+	fmt.Printf("hitesh details are: %+v\n", hitesh)
 }

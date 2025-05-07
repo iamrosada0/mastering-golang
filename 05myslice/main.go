12aaa095a5a2532091ad406f77261606074d82b4
iamrosada0 <luisrosada@outlook.com>
2025-05-07 14:57:52 +0100

    feat: slice is running

--- 05myslice/main.go
+++ 05myslice/main.go
@@ -6,11 +6,9 @@ func main() {
 	fmt.Println("Welcome to video on slices")
 
 	var fruitList = []string{"apple", "Tomato", "Peach"}
-
 	fmt.Printf("Type of fruitlist is %T\n", fruitList)
-	//adding items
 
+	// Adding items
 	fruitList = append(fruitList, "Mango", "Banana")
-
 	fmt.Println(fruitList)
 }

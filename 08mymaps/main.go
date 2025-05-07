2dd6d443203d8c1bf5d007c61a70070e5770c691
iamrosada0 <luisrosada@outlook.com>
2025-05-07 16:04:42 +0100

    feat: added deleted

--- 08mymaps/main.go
+++ 08mymaps/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println("Maps in Golang")
+
+	languages := make(map[string]string)
+
+	languages["js"] = "Javascript"
+	languages["rb"] = "Ruby"
+	languages["py"] = "Python"
+
+	fmt.Println("List of all language", languages)
+	fmt.Println("JS shorts for", languages["js"])
+
+	//removing from slice
+
+	delete(languages, "rb")
+	fmt.Println("List of all language after deleted", languages)
 
 }

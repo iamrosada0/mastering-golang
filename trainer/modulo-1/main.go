591f1d2c70ce9d1a96f9b6cefdd3b1c9509ebdaf
iamrosada0 <luisrosada@outlook.com>
2025-06-01 17:14:23 +0100

    feat: add other function

--- trainer/modulo-1/main.go
+++ trainer/modulo-1/main.go
@@ -11,3 +11,34 @@ func main() {
 
 	fmt.Println(idade, ativo, nome, altura)
 }
+
+func main() {
+	nome := "Carlos"
+	idade := 28
+	altura := 1.82
+	ativo := true
+
+	fmt.Printf("Nome: %s\n", nome)
+	fmt.Printf("Idade: %d\n", idade)
+	fmt.Printf("Altura: %.2f\n", altura)
+
+	fmt.Printf("Ativo: %b\n", ativo)
+}
+
+func main() {
+	const PI float64 = 3.14
+
+	const RAIO float64 = 5.0
+
+	area := PI * RAIO * RAIO
+	fmt.Printf("area: %.2f\n", area)
+
+}
+
+func main() {
+	var celsius = 30.0
+
+	F := (celsius * 1.8) + 32
+	fmt.Printf("celsius: %.2f\n", F)
+
+}
